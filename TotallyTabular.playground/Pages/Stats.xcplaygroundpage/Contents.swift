8800154ff1daa6e36555238a26742b54131af9b0
Quinn McHenry <quinn.mchenry@smallplanet.com>
2022-03-08 16:14:42 -0500

    Remove commented code after presentation

--- TotallyTabular.playground/Pages/Stats.xcplaygroundpage/Contents.swift
+++ TotallyTabular.playground/Pages/Stats.xcplaygroundpage/Contents.swift
@@ -8,7 +8,7 @@ print(mtcars.summary())
 
 print(mtcars.grouped(by: "cyl").means("mpg", Double.self, order: .descending))
 
-//mtcars.transformColumn(ColumnID("hp", Int.self)) { Double($0) }
+mtcars.transformColumn(ColumnID("hp", Int.self)) { Double($0) }
 print(mtcars.grouped(by: "cyl").means("hp", Double.self, order: .descending))
 
 //: [Next](@next)
